9d8e7f564ce2f953d710156427333cdbb3432680
Devohka <andr.fedak@gmail.com>
2024-08-31 13:08:43 +0300

    fdsf

--- src/App.js
+++ src/App.js
@@ -5,20 +5,85 @@ import './App.css';
 class App extends Component {
 
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    tellBook: [
+      { id: 1, name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 2, name: 'Hermione Kline', number: '443-89-12' },
+      { id: 3, name: 'Eden Clements', number: '645-17-79' },
+      { id: 4, name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
     name: '',
     number: ''
-  }
+  };
+
+  createNumder = () => {
+    return Math.round(Math.random() * (9999 - 5) + 5);
+  };
+
+  addNumber = (e) => {
+    e.preventDefault();
+    const event = e.target;
+    const contactsName = event.elements.name.value;
+    const contactsNumder = event.elements.number.value;
+    const newContact = {
+      name: contactsName,
+      number: contactsNumder,
+      id: this.createNumder(),
+    };
+    this.state.tellBook.find(contact => {
+      console.log(contact)
+      if (contact.name === newContact.name && contact.number === newContact.number) {
+        alert(`${newContact.name} is already in contacts`);
+
+      } else {
+        this.setState({ tellBook: [...this.state.tellBook, newContact] });
+      };
+    });
+
+    // this.setState(
+    //     (preven) => {
+    //         return {
+    //              tellBook: preven.tellBook.push(newContact),
+    //         };
+    //     }
+    // )
+    e.target.reset();
+
+  };
+
+  findTellBook = (e) => {
+    const valueinput = e.target.value;
+    const findValueTellBook = this.state.tellBook.filter(item => {
+      return item.name === valueinput;
+    });
+    console.log(findValueTellBook);
+
+    this.setState({ tellBook: findValueTellBook });
+
+
+  };
+
+  deleteContact = (e) => {
+    const idToDelete = parseInt(e.target.closest("li")["id"]);
+    const newTellBook = this.state.tellBook.filter(item => {
+
+      return item.id !== idToDelete;
+    });
+    console.log(idToDelete);
+    newTellBook.splice(idToDelete, 0)
+    this.setState({ tellBook: newTellBook });
+  };
   render() {
+    const { tellBook } = this.state;
+    console.log(tellBook);
     return (
       <>
-      <TellBook></TellBook>
+        <TellBook 
+        addNumber={this.addNumber}
+        findTellBook={this.findTellBook}
+        deleteContact={this.deleteContact}
+        tellBook={this.state.tellBook}
+        ></TellBook>
       </>
     );
   };
--- src/Component/TellBook.jsx
+++ src/Component/TellBook.jsx
@@ -7,7 +7,7 @@ const Text = styled.h2`
     margin-left: 15px;
 `;
 
-const Input = styled.input`
+const InputFind = styled.input`
     margin-left: 15px;
 `;
 
@@ -51,86 +51,13 @@ height: 200px;
   }  
 `;
 
-const safeArr = [];
-function createUniqueId() {
-  const id = "id" + Math.random().toString(16).slice(2);
-  if (safeArr.includes(id)) {
-    createUniqueId();
-  } else {
-    safeArr.push(id);
-    return id;
-  }
-}
 
 
-class TellBook extends Component {
+function TellBook ({addNumber, findTellBook, deleteContact, tellBook}) {
 
-    state = {
-        // tellBook: this.props,
-        // contacts
-        tellBook: [
-            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ],
-    };
-
-    // createNumder = () => {
-    //     let numberTell = 0;
-    //     return numberTell = Math.round(Math.random() * (999 - 100) + 100) + "-" +
-    //         Math.round(Math.random() * (99 - 10) + 10) + "-" +
-    //         Math.round(Math.random() * (99 - 10) + 10);
-    // };
-
-    addNumber = (e) => {
-        e.preventDefault();
-        const event = e.target;
-        const contactsName = event.elements.name.value;
-        const contactsNumder = event.elements.number.value;
-        const newContact = {
-            name: contactsName,
-            number: contactsNumder,
-            id: createUniqueId(),
-        };
-        this.state.tellBook.find(contact => {
-            console.log(contact)
-            if (contact.name === newContact.name && contact.number === newContact.number) {
-                alert(`${newContact.name} is already in contacts`);
-            } else {
-                this.setState({ tellBook: [...this.state.tellBook, newContact] });
-            }
-        })
-
-        // this.setState(
-        //     (preven) => {
-        //         return {
-        //              tellBook: preven.tellBook.push(newContact),
-        //         };
-        //     }
-        // )
-        e.target.reset();
-
-    };
-
-
-
-    deleteContact = (e) => {
-        const idToDelete = parseInt(e.target.closest("li")["id"]);
-        const newTellBook = this.state.tellBook.filter(item => {
-            // console.log(this.state.tellBook.splice(idToDelete, 1));
-          return item.id !== idToDelete;
-        });
+        //    console.log(...state.tellBook.tellBook);
+        // console.log(createNumder());
         
-        this.setState({ tellBook: newTellBook });
-      }
-
-    render() {
-        //    console.log(...this.state.tellBook.tellBook);
-        // console.log(this.createNumder());
-        const { tellBook } = this.state;
-
-        console.log(tellBook);
         return (
             <>
                 <Text>
@@ -138,7 +65,7 @@ class TellBook extends Component {
                 </Text>
 
                 <BoxContacts>
-                    <form onSubmit={this.addNumber}>
+                    <form onSubmit={addNumber}>
                         <h2>Name</h2>
                         <input
                             type="text"
@@ -163,9 +90,9 @@ class TellBook extends Component {
                 <Text>
                     Find contacts by name
                 </Text>
-                <Input
-                    type="text"
-                />
+
+                <InputFind onInput={findTellBook} type="text" />
+
 
                 <List>
                     {tellBook.map(contact => {
@@ -174,7 +101,7 @@ class TellBook extends Component {
                                 <li id={contact.id} key={contact.id}>
                                     <p>{contact.name}</p>
                                     <p>{contact.number}</p>
-                                    <button type="button" onClick={this.deleteContact}>Delete</button>
+                                    <button onClick={deleteContact}>Delete</button>
                                 </li>
                             </>
                         );
@@ -184,6 +111,6 @@ class TellBook extends Component {
         );
     };
 
-};
+
 
 export default TellBook;
